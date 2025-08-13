ecb597d5c3df04ea32fb2ab19b10606a9e26eaf8
umernadim <itsumernadeem@gmail.com>
2025-08-14 00:37:30 +0500

    updated

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
